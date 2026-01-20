813618b686046af4245cb9d019d94d4f15f75e5c
Michal Prihoda <michal@prihoda.net>
2026-01-20 21:59:32 +0100

    Dep update

--- .iw/core/project.scala
+++ .iw/core/project.scala
@@ -3,7 +3,7 @@
 
 //> using scala 3.3.7
 //> using dep com.typesafe:config:1.4.5
-//> using dep com.softwaremill.sttp.client4::core:4.0.13
+//> using dep com.softwaremill.sttp.client4::core:4.0.14
 //> using dep com.lihaoyi::upickle:4.4.2
 //> using dep com.lihaoyi::os-lib:0.11.6
 //> using dep com.lihaoyi::cask:0.11.3
